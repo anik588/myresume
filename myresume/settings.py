3a8c25f5bcde29e1c042441adef5d8a1deb778e3
Sajjad Ahmed Anik <sajjad.anik24@gmail.com>
2025-05-06 13:20:48 +0600

    Fix wsgi.py path and Procfile

--- myresume/settings.py
+++ myresume/settings.py
@@ -82,6 +82,8 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/admin/User/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_TRUSTED_ORIGINS = [
     "https://sajjad-anik.up.railway.app"
 ]
