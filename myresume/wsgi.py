6911114991540ccc0d9a41be9cf200a7c56f5a2a
SA-Anik <sajjad.anik24@gmail.com>
2026-04-01 03:09:16 +0600

    Add .gitignore and update wsgi.py for FTP deployment

--- myresume/wsgi.py
+++ myresume/wsgi.py
@@ -1,12 +1,16 @@
-"""
-WSGI config for myresume project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
 import os
+import sys
+
+# project path
+project_home = "/home/sajjadan/repositories/myresume"
+
+# add to python path
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myresume.settings")
+
+# start django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myresume.settings')
-
 application = get_wsgi_application()
